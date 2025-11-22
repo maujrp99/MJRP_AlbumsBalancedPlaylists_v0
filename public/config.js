62179e01c676d539cc60ae8c027743b88e9237fa
maujrp99 <maujrp@gmail.com>
2025-11-22 18:37:57 -0300

    chore: remove legacy public/config.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,7 @@
-window.__firebase_config = {
-    apiKey: "AIzaSyA8x-II628CK3qQmmfr-WJE13LXdu7nAxs",
-    authDomain: "mjrp-playlist-generator.firebaseapp.com",
-    projectId: "mjrp-playlist-generator",
-    storageBucket: "mjrp-playlist-generator.firebasestorage.app",
-    messagingSenderId: "540062660076",
-    appId: "1:540062660076:web:f2cd9d6c24d9dd702ffcc9"
-};
-console.log("Config.js loaded. window.__firebase_config:", window.__firebase_config);
+// REMOVED: This file was intentionally cleared. Use `./js/firebase-config.js` as the canonical
+// Firebase configuration. Keeping an empty stub preserves backward-compatibility for file
+// references while avoiding duplicated credentials in multiple files.
+
+// If you want this file deleted from the repository entirely, run:
+//   git rm public/config.js && git commit -m "chore: remove legacy config.js"
+
