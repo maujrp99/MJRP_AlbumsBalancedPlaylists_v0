b70173bc62a599a6a9a3e8382f2106fed08dc019
maujrp99 <maujrp@gmail.com>
2025-11-22 14:42:08 -0300

    fb_config updated

---
+++ public/config.js
@@ -0,0 +1,9 @@
+window.__firebase_config = {
+    apiKey: "AIzaSyA8x-II628CK3qQmmfr-WJE13LXdu7nAxs",
+    authDomain: "mjrp-playlist-generator.firebaseapp.com",
+    projectId: "mjrp-playlist-generator",
+    storageBucket: "mjrp-playlist-generator.firebasestorage.app",
+    messagingSenderId: "540062660076",
+    appId: "1:540062660076:web:f2cd9d6c24d9dd702ffcc9"
+};
+console.log("Config.js loaded. window.__firebase_config:", window.__firebase_config);
---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+window.__firebase_config = {
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "mjrp-playlist-generator.firebaseapp.com",
+    projectId: "mjrp-playlist-generator",
+    storageBucket: "mjrp-playlist-generator.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+console.log("Config.js loaded. window.__firebase_config:", window.__firebase_config);
