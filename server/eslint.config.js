e60ed71821ee2610c2f0577428c399c5a6cd19c4
maujrp99 <maujrp@gmail.com>
2025-11-22 17:07:17 -0300

    chore: add ESLint and Prettier to server

---
+++ server/eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    ignores: [
+      'node_modules/',
+      'logs/',
+      '/tmp/',
+      '*.log',
+      'package-lock.json',
+      'public/'
+    ]
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'script'
+    },
+    rules: {
+      'no-console': 'off'
+    }
+  }
+];
